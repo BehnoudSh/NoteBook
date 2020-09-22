2a71d579ffae83727eff7b7bbe5f4bce7cf2ec6e
b.sherafati <behnoud.sherafati@gmail.com>
2020-09-22 14:18:43 +0330

    refactor

---
+++ app/src/main/java/ir/navaco/behnoudnote/views/AddNoteActivity.kt
@@ -0,0 +1,13 @@
+package ir.navaco.behnoudnote.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+ import ir.navaco.behnoudnote.R
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addnote)
+
+    }
+}
