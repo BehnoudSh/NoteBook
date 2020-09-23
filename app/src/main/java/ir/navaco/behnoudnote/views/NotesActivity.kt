9d74e2774ec98e943da2eb2442b0d3666bb2b8d4
b.sherafati <behnoud.sherafati@gmail.com>
2020-09-23 14:03:55 +0330

    all the modules are ready such as:
    entity
    room
    repo
    livedata
    viewmodel
    observer in UI

--- app/src/main/java/ir/navaco/behnoudnote/views/NotesActivity.kt
+++ app/src/main/java/ir/navaco/behnoudnote/views/NotesActivity.kt
@@ -3,12 +3,10 @@ package ir.navaco.behnoudnote.views
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
