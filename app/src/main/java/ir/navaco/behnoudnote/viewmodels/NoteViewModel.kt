6fc1ceeadacada99cd1d30a45aa317a5dc179b2d
b.sherafati <behnoud.sherafati@gmail.com>
2020-09-23 14:28:12 +0330

    adding kotlin coroutines for inserting in room db

--- app/src/main/java/ir/navaco/behnoudnote/viewmodels/NoteViewModel.kt
+++ app/src/main/java/ir/navaco/behnoudnote/viewmodels/NoteViewModel.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import ir.navaco.behnoudnote.data.AppDataBase
 import ir.navaco.behnoudnote.data.Note
 import ir.navaco.behnoudnote.data.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 public class NoteViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -22,7 +25,11 @@ public class NoteViewModel(application: Application) : AndroidViewModel(applicat
         return noteRepository.getNotes()
     }
 
-    fun insertNote(note: Note) {
+//    fun insertNote(note: Note) {
+//        noteRepository.addNote(note)
+//    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
         noteRepository.addNote(note)
     }
 
